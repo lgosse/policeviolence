2abe780118147cb952fd26e4938924d1de092f90
Lucas Gossé <lucas@side.co>
2020-06-01 15:52:53 +0200

    update

--- src/components/ViolenceItem.tsx
+++ src/components/ViolenceItem.tsx
@@ -11,6 +11,7 @@ interface Violence {
   age?: number;
   picture?: string;
   location: string;
+  state: string;
   description: string;
   disposition: string;
   article: string;
@@ -22,23 +23,30 @@ const ViolenceItem = (props: Props) => {
 
   return (
     <div className="violence-item">
-      <h3 className="violence-item__name">
-        {violence.name}, {violence.age}
-      </h3>
-      {
-        <p className="violence-item__location">
-          {violence.location}, {new Date(violence.date).toLocaleString()}
+      <div className="violence-item__container">
+        <h3 className="violence-item__name">
+          {violence.name}, {violence.age}
+        </h3>
+        {
+          <p className="violence-item__location">
+            {violence.location}, {violence.state}, {new Date(violence.date).toLocaleString()}
+          </p>
+        }
+        <p className="violence-item__description">
+          {violence.description}
         </p>
-      }
-      <p className="violence-item__description">
-        {violence.description}
-      </p>
-      <p className="violence-item__sources">
-        Article:&nbsp;
-        <a href={violence.article} target="_blank" rel="noopener noreferrer">
-          {violence.article.replace(/https:\/\//, '').replace(/\/.+/, '')}
-        </a>
-      </p>
+        <p className="violence-item__sources">
+          Article:&nbsp;
+          <a href={violence.article} target="_blank" rel="noopener noreferrer">
+            {violence.article.replace(/https?:\/\//, '').replace(/\/.+/, '')}
+          </a>
+        </p>
+      </div>
+      <div className="violence-item__picture">
+        {violence.picture &&
+          <img src={violence.picture} alt={violence.name} />
+        }
+      </div>
     </div>
   );
 }
